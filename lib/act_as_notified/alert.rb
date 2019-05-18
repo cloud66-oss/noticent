f4c3100a097b7d75b6ae0f2482222998a6f99ad3
Khash Sajadi <khash@cloud66.com>
2019-05-18 13:56:40 +0100

    payload
    Signed-off-by: Khash Sajadi <khash@cloud66.com>

--- lib/act_as_notified/alert.rb
+++ lib/act_as_notified/alert.rb
@@ -15,30 +15,14 @@ module ActAsNotified
       @scope = scope
     end
 
-    class Notifier
-
-      attr_reader :recipient
-      attr_reader :group
-
-      def initialize(config, recipient)
-        @recipient = recipient
-        @config = config
-      end
-
-      def on(group)
-        @group = group
-      end
-
-    end
-
-    def notify(recipient)
+    def notify(recipient, template: '')
       raise BadConfiguration, 'no recipients are defined yet' if @config.recipients.nil?
       raise BadConfiguration, "recipient '#{recipient}' not defined" unless @config.recipients.include? recipient
 
       notifiers = @config.instance_variable_get(:@notifiers) || {}
       raise BadConfiguration, "a notify is already defined for '#{recipient}'" unless notifiers[recipient].nil?
 
-      notifiers[recipient] = ActAsNotified::Alert::Notifier.new(@config, recipient)
+      notifiers[recipient] = ActAsNotified::Alert::Notifier.new(@config, recipient, template: template)
       @config.instance_variable_set(:@notifiers, notifiers)
 
       notifiers[recipient]
@@ -54,6 +38,24 @@ module ActAsNotified
       raise BadConfiguration, 'cannot have all mixed with other scopes' if scope.include?(:all) && scope.count != 1
 
       raise BadConfiguration, "invalid or undefined scope. valid values are #{config.scopes.values.keys.join(', ')} and all" unless (scope - config.scopes.values.keys).empty?
+    end
+
+    class Notifier
+
+      attr_reader :recipient
+      attr_reader :group
+      attr_reader :template
+
+      def initialize(config, recipient, template: '')
+        @recipient = recipient
+        @config = config
+        @template = template
+      end
+
+      def on(group)
+        @group = group
+      end
+
     end
 
   end
---
+++ lib/act_as_notified/payload.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ActAsNotified
+  class Payload
+
+  end
+end
