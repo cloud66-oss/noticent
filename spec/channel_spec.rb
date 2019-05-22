1d2e8737a1d2c3ba64a9fa738c598f53b9539c90
Khash Sajadi <khash@cloud66.com>
2019-05-22 11:34:34 -0700

    split channel def and channel tests
    Signed-off-by: Khash Sajadi <khash@cloud66.com>

--- spec/channel_spec.rb
+++ spec/channel_spec.rb
@@ -1,42 +1,31 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe Noticent::Definitions::Channel do
+describe Noticent::Channel do
 
-  it 'can be configured' do
-    ch = Noticent::Definitions::Channel.new(Noticent.configuration, :foo, group: :test, klass: Integer)
+  it 'should have protected properties' do
+    ch = Noticent::Channel.new([], {foo: :bar}, {})
 
-    expect(ch.klass).to eq(Integer)
-    expect(ch.name).to eq(:foo)
-    expect(ch.group).to eq(:test)
+    expect(ch).not_to be_nil
+    expect(ch.send(:recipients)).not_to be_nil
+    expect(ch.send(:payload)).not_to be_nil
+    expect(ch.send(:context)).not_to be_nil
   end
 
-  it 'should have a default group' do
-    ch = Noticent::Definitions::Channel.new(Noticent.configuration, :foo)
-    expect(ch.group).to eq(:default)
+  it 'should have current_user' do
+    ch = Noticent::Channel.new([], { foo: :bar }, current_user: :bar)
+
+    expect(ch.send(:context)[:current_user]).not_to be_nil
+    expect(ch.send(:context)[:current_user]).to eq(:bar)
+    expect(ch.send(:current_user)).to eq(:bar)
   end
 
-  it 'should support custom classes' do
-    Noticent.configure {}
-    ch = Noticent::Definitions::Channel.new(Noticent.configuration, :foo)
-    expect(ch.klass).to eq(Noticent::Samples::Foo)
-  end
+  it 'should raise exception if no current user available' do
+    ch = Noticent::Channel.new([], { foo: :bar }, buzz: :fuzz)
 
-  it 'should expect channel to inherit from ::Noticent::Channel' do
-    class BadChannel; end
-
-    expect do
-      Noticent.configure do
-        channel(:bad_channel) { configure(BadChannel) }
-      end
-    end.to raise_error Noticent::BadConfiguration
-  end
-
-  it 'should use the right class for channel' do
-    expect do
-      Noticent.configure do
-        channel(:some_channel) {}
-      end
-    end.to raise_error Noticent::BadConfiguration
+    expect { ch.send(:current_user) }.to raise_error Noticent::NoCurrentUser
   end
 
 end
+
---
+++ spec/definitions/channel_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Noticent::Definitions::Channel do
+
+  it 'can be configured' do
+    ch = Noticent::Definitions::Channel.new(Noticent.configuration, :foo, group: :test, klass: Integer)
+
+    expect(ch.klass).to eq(Integer)
+    expect(ch.name).to eq(:foo)
+    expect(ch.group).to eq(:test)
+  end
+
+  it 'should have a default group' do
+    ch = Noticent::Definitions::Channel.new(Noticent.configuration, :foo)
+    expect(ch.group).to eq(:default)
+  end
+
+  it 'should support custom classes' do
+    Noticent.configure {}
+    ch = Noticent::Definitions::Channel.new(Noticent.configuration, :foo)
+    expect(ch.klass).to eq(Noticent::Samples::Foo)
+  end
+
+  it 'should expect channel to inherit from ::Noticent::Channel' do
+    class BadChannel; end
+
+    expect do
+      Noticent.configure do
+        channel(:bad_channel) { configure(BadChannel) }
+      end
+    end.to raise_error Noticent::BadConfiguration
+  end
+
+  it 'should use the right class for channel' do
+    expect do
+      Noticent.configure do
+        channel(:some_channel) {}
+      end
+    end.to raise_error Noticent::BadConfiguration
+  end
+
+end
