47542c66b2ffd194b790198b3534020274e5f664
dima <dima@cloud66.com>
2023-12-06 11:47:40 +0000

    Cache rails_7_or_greater

--- lib/noticent/rails_version.rb
+++ lib/noticent/rails_version.rb
@@ -1,7 +1,8 @@
 module Noticent
   class RailsVersion
     def self.rails_7_or_greater?
-      ::Gem.loaded_specs["rails"].version.to_s.to_i >= 7
+      return @rails_7_or_greater unless @rails_7_or_greater.nil?
+      @rails_7_or_greater = ::Gem.loaded_specs["rails"].version.to_s.to_i >= 7
     end
   end
 end
